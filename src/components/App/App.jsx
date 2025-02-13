d951e385537947bb46c5b0a830e9e2e2610e7410
Barry Richards <barryrichards05@gmail.com>
2025-02-13 13:52:25 +0000

    improved layout with CSS

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,22 +5,26 @@ import SearchResults from '../SearchResults/SearchResults'
 import Playlist from '../Playlist/Playlist'
 
 function App() {
-  const [searchResults, setSearchResults] = useState([{
-    id: 1, artist: "Bastille", title: "Pompeii", album: "Bad Blood"
-  },
-  {
-    id: 2, artist: "Orville Peck", title: "Daytona Sand", album: "Bronco"
-  },
-  {
-    id: 3, artist: "Bring Me the Horizon", title: "Sleepwalking", album: "Sempiternal"
-  }]);
+  const [searchResults, setSearchResults] = useState([
+    {
+      id: 1, artist: "Bastille", title: "Pompeii", album: "Bad Blood"
+    },
+    {
+      id: 2, artist: "Orville Peck", title: "Daytona Sand", album: "Bronco"
+    },
+    {
+      id: 3, artist: "Bring Me the Horizon", title: "Sleepwalking", album: "Sempiternal"
+    }
+  ]);
 
   return (
     <div className={styles.app}>
-    <h1>Ja<span className={styles.highlight}>mmm</span>ing</h1>
-    <SearchBar />
-    <SearchResults searchResults={searchResults} />
-    <Playlist />
+      <header>
+        <h1 className={styles.header}>Ja<span className={styles.highlight}>mmm</span>ing</h1>
+      </header>
+      <SearchBar />
+      <SearchResults searchResults={searchResults} />
+      <Playlist />
     </div>
   )
 }

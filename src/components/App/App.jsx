b9791bb14135195625cebe63cd2bef2af03a52d3
Barry Richards <barryrichards05@gmail.com>
2025-02-13 10:06:53 +0000

    create components directories and added jsx and css module files to each

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import './App.module.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <p>Hellooooooooo</p>
+  )
+}
+
+export default App
